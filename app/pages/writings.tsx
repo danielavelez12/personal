c79eaf332e7708b60aff798f1d7124aa90ae68a1
Daniela Velez <danielavelez1201@gmail.com>
2021-12-22 01:49:41 -0500

    Implement writing display

--- app/pages/writings.tsx
+++ app/pages/writings.tsx
@@ -1,18 +1,49 @@
 import { NextPage } from "next";
 import Script from "next/script";
+import path from "path";
 import { NavBar } from "./components/Navbar";
+import fs from "fs";
+import matter from "gray-matter";
+import { remark } from "remark";
+import html from "remark-html";
 
-const Writings: NextPage = () => {
+const writingsDirectory = path.join(process.cwd(), "writings");
+
+function Writings({ writings }) {
+  console.log(writings);
   return (
     <>
-      <Script src="https://www.retainable.io/assets/retainable/rss-embed/retainable-rss-embed.js"></Script>
       <NavBar></NavBar>
 
       <div className="container mx-auto px-12 py-5 h-screen bg-white">
         <h1 className=" px-3 text-3xl font-bold text-med">writings</h1>
+        <div
+          className="text-md"
+          dangerouslySetInnerHTML={{ __html: writings }}
+        />
+        <ul>
+            <li>hi</li>
+        </ul>
       </div>
     </>
   );
-};
+}
+
+export async function getStaticProps() {
+  const fullPath = path.join(writingsDirectory, `sample.md`);
+  const fileContents = fs.readFileSync(fullPath, "utf8");
+  const matterResult = matter(fileContents);
+
+  const processedContent = await remark()
+    .use(html)
+    .process(matterResult.content);
+  const writings = processedContent.toString();
+
+  return {
+    props: {
+      writings,
+    },
+  };
+}
 
 export default Writings;
