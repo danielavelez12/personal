2b0c45d5b4c8d4bbb1c7f0bb148f15bbcb43af73
Daniela Velez <danielavelez1201@gmail.com>
2021-12-22 05:07:39 -0500

    Change image to be relative import

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -5,6 +5,7 @@ import { Socials } from "../components/Socials";
 import { Intro } from "../components/Intro";
 import { List } from "../components/List";
 import { NavBar } from "../components/Navbar";
+import portrait from "../imgs/portrait.jpeg";
 
 const Home: NextPage = () => {
   return (
@@ -13,7 +14,7 @@ const Home: NextPage = () => {
 
       <div className="container mx-auto px-12 py-5 h-screen bg-white">
         <div className="flex py-6 items-end">
-          <Image src="/portrait.jpeg" width={50} height={50} />
+          <Image src={portrait} width={50} height={50} />
           <h1 className=" px-3 text-3xl font-bold text-med">
             {"Hi! I'm Daniela."}
           </h1>
