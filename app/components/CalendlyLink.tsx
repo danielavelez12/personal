ca7d6a9b24f94eba387d7c562764e037066fb416
Daniela Velez <danielavelez1201@gmail.com>
2021-12-23 10:34:37 -0500

    Edit calendly link

--- app/components/CalendlyLink.tsx
+++ app/components/CalendlyLink.tsx
@@ -12,33 +12,10 @@ export const CalendlyLink = () => {
   };
 
   const prefill = {
-    email: "test@test.com",
-    firstName: "Jon",
-    lastName: "Snow",
-    name: "Jon Snow",
-    guests: ["janedoe@example.com", "johndoe@example.com"],
-    customAnswers: {
-      a1: "a1",
-      a2: "a2",
-      a3: "a3",
-      a4: "a4",
-      a5: "a5",
-      a6: "a6",
-      a7: "a7",
-      a8: "a8",
-      a9: "a9",
-      a10: "a10",
-    },
     date: new Date(Date.now() + 86400000),
   };
 
-  const utm = {
-    utmCampaign: "Spring Sale 2019",
-    utmContent: "Shoe and Shirts",
-    utmMedium: "Ad",
-    utmSource: "Facebook",
-    utmTerm: "Spring",
-  };
+  const utm = {};
 
   const url = "https://calendly.com/danielavelez/chat";
 
@@ -47,7 +24,7 @@ export const CalendlyLink = () => {
   return (
     <button onClick={onClick} className="py-2 w-fit">
       <p className="text-md text-med hover:text-blue hover:underline">
-        ☕️ Grab a time on my calendar to say hi!
+        ☕️ Grab a time to say hi!
       </p>
     </button>
   );
