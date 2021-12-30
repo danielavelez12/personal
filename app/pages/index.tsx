c06742a3767212524ad6bc1e82499e4d10de7644
Daniela Velez <danielavelez1201@gmail.com>
2021-12-30 00:41:54 -0500

    Favicon

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -10,19 +10,21 @@ import { CalendlyLink } from "../components/CalendlyLink";
 const Home: NextPage = () => {
   return (
     <>
-      <NavBar></NavBar>
+      <div>
+        <NavBar></NavBar>
 
-      <div className="container mx-auto px-12 py-5 h-screen bg-white">
-        <div className="flex py-6 items-end">
-          <Image src="/portrait.png" width={50} height={50} />
-          <h1 className=" px-3 text-3xl font-bold text-med">
-            {"Hi! I'm Daniela."}
-          </h1>
+        <div className="container mx-auto px-12 py-5 h-screen bg-white">
+          <div className="flex py-6 items-end">
+            <Image src="/portrait.png" width={50} height={50} />
+            <h1 className=" px-3 text-3xl font-bold text-med">
+              {"Hi! I'm Daniela."}
+            </h1>
+          </div>
+          <Intro></Intro>
+          <CalendlyLink></CalendlyLink>
+          <Socials></Socials>
+          <List></List>
         </div>
-        <Intro></Intro>
-        <CalendlyLink></CalendlyLink>
-        <Socials></Socials>
-        <List></List>
       </div>
     </>
   );
